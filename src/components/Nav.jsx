781051c96e2109ff336ed645801cfaf5cd4bd8c4
Tony Sulfaro <tony116523@gmail.com>
2020-03-31 15:41:48 -0400

    generate subreddit links in nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-import Navbar from "react-bootstrap/Navbar";
+import React, { useState, useEffect } from "react";
+import { Navbar, NavDropdown } from "react-bootstrap";
 
 const Nav = props => {
+  const [subreddits, setSubreddits] = useState([]);
+
+  useEffect(() => {
+    async function getSubreddits() {
+      const response = await fetch("https://www.reddit.com/subreddits/.json");
+      const json = await response.json();
+      console.log(json.data.children);
+      setSubreddits(json.data.children);
+    }
+
+    getSubreddits();
+  }, []);
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand href="#home">Reddit Client</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <NavDropdown title="Subreddit" id="basic-nav-dropdown">
+        {subreddits.map(subreddit => (
+          <NavDropdown.Item href="#action/3.3">
+            {subreddit.data.title}
+          </NavDropdown.Item>
+        ))}
+      </NavDropdown>
     </Navbar>
   );
 };
